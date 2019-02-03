f919b50d5a5de2109e013828088c6a9466f89908
Matheus Palma <mmatheuspalma@gmail.com>
2019-02-03 01:40:51 -0200

    Minor changes

--- src/redux/Restaurant/services.ts
+++ src/redux/Restaurant/services.ts
@@ -1,6 +1,5 @@
-import API from '../../helpers/api';
-
-import { get } from '../../helpers/agent';
+import API from 'helpers/api';
+import { get } from 'helpers/agent';
 
 const listRestaurant = (params: any) => get(API.restaurant.list, params);
 const loadRestaurantById = (id: any) => get(API.restaurant.detail.replace('{id}', id));
